8b2cf515edc1933b59ae588942070089b5836395
przybysh <przybysh@gmail.com>
2025-02-20 11:23:24 +0100

    Renamed file with main function. Removed install section from CMakeLists.txt Added content to README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Substrate_SDL3
 Implementation of greate substrate screen saver in SDL3.
+
+## Where it comes from
+Implementation is based on a [SDL3 hello](https://github.com/libsdl-org/SDL/blob/main/docs/hello.c) and [Haiku](https://github.com/stephanie-fu/Substrate) implementation of substrate algorithm.
+
+
+Do not forget to visit webpage of a original author of substrate algorithm:
+[Jared Tarbell](http://www.complexification.net/gallery/machines/substrate/) 
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
 /*
-  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely.
 */
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
